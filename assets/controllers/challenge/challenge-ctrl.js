87d9dad4efb291641c22231091f5d65b1dfa1264
heavyrajn <truongvanlinh2012@gmail.com>
2014-07-28 17:40:04 +0700

    Update test 3 version 1

--- assets/controllers/challenge/challenge-ctrl.js
+++ assets/controllers/challenge/challenge-ctrl.js
@@ -80,7 +80,7 @@ angular.module('arena.challenge.controller', [
         };
 
         $http.get('/data/myData.json').success(function (data) {
-            quizMachine = new QuizStateMachine(data.slice(63,73), self);
+            quizMachine = new QuizStateMachine(data.slice(33,43), self);
         });
         $scope.getProgress=function(){
           return (($scope.countDown)/$scope.timeout*100)+'%';

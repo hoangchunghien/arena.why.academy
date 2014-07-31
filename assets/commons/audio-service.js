196ed32dcaf5de11c38fac0f1cc27d4b6c7cb647
heavyrajn <truongvanlinh2012@gmail.com>
2014-07-31 16:28:41 +0700

    Fix some bugs version 1

--- assets/commons/audio-service.js
+++ assets/commons/audio-service.js
@@ -5,38 +5,119 @@
 angular.module('arena.audio.service', [
 
 ])
-    .service('audioSrv', function() {
-        this.playAudio = function(url) {
+    .service('audioSrv', function () {
+        var backgroundAudio;
+        var countDownAudio;
+        var countDownCoongAudio;
+        var wrongAnswerAudio;
+        var correctAnswerAudio;
+
+        this.playAudio = function (url) {
             var mySound = soundManager.createSound({
                 url: url
             });
             mySound.play();
         };
 
-        this.checkSoundUrl = function(url) {
+        this.checkSoundUrl = function (url) {
             var callback = arguments[1];
             var audio = new Audio();
             var timer;
-            audio.oncanplay = function() {
+            audio.oncanplay = function () {
                 clearTimeout(timer);
                 if (callback) callback(true);
             };
-            audio.onerror = function() {
+            audio.onerror = function () {
                 clearTimeout(timer);
                 if (callback) callback(false);
             };
             audio.src = url;
-            timer = setTimeout(function() {
+            timer = setTimeout(function () {
                 audio.src = url;
                 timer;
             }, 3000);
         };
         //
-        this.createAudio=function(url,loop){
-            return soundManager.createSound({
-                url: url,
-                loops:loop
-            });
+        this.getBackgroundAudio = function () {
+            var url = arguments[0];
+            var loop = arguments[1];
+            if (backgroundAudio == null) {
+                backgroundAudio = soundManager.createSound({
+                    id: 'backgroundAudio',
+                    url: url,
+                    loops: loop
+                });
+            }
+            return backgroundAudio;
         };
+        this.setBackgroundAudio = function (sound) {
+            backgroundAudio = sound;
+        };
+        //
+        this.getCountDownAudio = function () {
+            var url = arguments[0];
+            if (countDownAudio == null) {
+                countDownAudio = soundManager.createSound({
+                    id: 'countDownAudio',
+                    url: url
+                });
+            }
+            return countDownAudio;
+        };
+        this.setCountDownAudio = function (sound) {
+            countDownAudio = sound;
+        };
+        //
+        this.getCountDownCoongAudio = function () {
+            var url = arguments[0];
+            if (countDownCoongAudio == null) {
+                countDownCoongAudio = soundManager.createSound({
+                    id: 'countDownCoongAudio',
+                    url: url
+                });
+            }
+            return countDownCoongAudio;
+        };
+        this.setCountDownCoongAudio = function (sound) {
+            countDownCoongAudio = sound;
+        };
+        //
+        this.getCorrectAnswerAudio = function () {
+            var url = arguments[0];
+            if (correctAnswerAudio == null) {
+                correctAnswerAudio = soundManager.createSound({
+                    id: 'correctAnswerAudio',
+                    url: url
+                });
+            }
+            return correctAnswerAudio;
+        };
+        this.setCorrectAnswerAudio = function (sound) {
+            correctAnswerAudio = sound;
+        };
+        //
+        this.getWrongAnswerAudio = function () {
+            var url = arguments[0];
+            if (wrongAnswerAudio == null) {
+                wrongAnswerAudio = soundManager.createSound({
+                    id: 'wrongAnswerAudio',
+                    url: url
+                });
+            }
+            return wrongAnswerAudio;
+        };
+        this.setWrongAnswerAudio = function (sound) {
+            wrongAnswerAudio = sound;
+        };
+
+        this.destroyAllSound = function () {
+            soundManager.destroySound('backgroundAudio');
+            soundManager.destroySound('countDownAudio');
+            soundManager.destroySound('countDownCoongAudio');
+            soundManager.destroySound('wrongAnswerAudio');
+            soundManager.destroySound('correctAnswerAudio');
+
+        };
+
 
     });

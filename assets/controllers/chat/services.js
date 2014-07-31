46d094a5333b79b4895201d8f715e8fb49399530
Viet Vo <tmimotw@outlook.com>
2014-07-31 10:09:31 +0700

    chat

---
+++ assets/controllers/chat/services.js
@@ -0,0 +1,33 @@
+﻿/// <reference path="Scripts/angular.js" />
+/// <reference path="app.js" />
+/// <reference path="js/socket.io-1.0.6.js" />
+app.factory('socket', function ($rootScope) {
+    var socket = io.connect('localhost:9092');
+    var disconnect = false;
+    return {
+        on: function (eventName, callback) {
+            socket.on(eventName, function () {
+                var args = arguments;
+                if (!disconnect) {
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                }
+            });
+        },
+        emit: function (eventName, data, callback) {
+            socket.emit(eventName, data, function () {
+                var args = arguments;
+                $rootScope.$apply(function () {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            });
+        },
+        disconnect: function () {
+            disconnect = true;
+            socket.disconnect();
+        }
+    }
+});

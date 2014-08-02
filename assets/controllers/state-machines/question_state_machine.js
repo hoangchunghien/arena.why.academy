e98ae7e589571b9830ce6a40e447a230c9d42b13
Hiền Hoàng <hoangchunghien@gmail.com>
2014-08-02 17:10:38 +0700

    fix timer to milisecond

--- assets/controllers/state-machines/question_state_machine.js
+++ assets/controllers/state-machines/question_state_machine.js
@@ -118,7 +118,7 @@ function QuestionStateMachine(question, eventListener) {
                 var timeForChangeQuestion;
                 if (self.answer == self.question.answer) {
                     self.correct = true;
-                    self.score = self.remainingTime;
+                    self.score = self.remainingTime / 1000;
                     timeForChangeQuestion = 3500;
                 } else {
                     self.score = 0;
